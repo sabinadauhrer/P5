4dfa184a556f052ecff5a322bb00606641a78ca8
spgfn <simon.pietruska@gfn.education>
2025-02-10 14:00:50 +0100

    Add files via upload

--- User.py
+++ User.py
@@ -2,11 +2,12 @@ import Address
 Address=Address.Address
 
 class User:
-    def __init__(self, username, name, firstname, email, iban, Address):
+    def __init__(self, username, name, firstname, email, phone, iban, Address):
         self.username=username
         self.name=name
         self.firstname=firstname
         self.email=email
+        self.phone=phone
         self.iban=iban
         self.Address=Address
         
