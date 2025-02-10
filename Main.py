44170c61463b3a4682943d1ba59eb05536a82768
spgfn <simon.pietruska@gfn.education>
2025-02-10 14:34:27 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -6,7 +6,45 @@ import math
 import User
 import Admin
 import Address
+import Database
+import Seed
+import UpdateDB
 
 User=User.User
 Admin=Admin.Admin
 Address=Address.Address
+
+def createDB():
+    try:
+        
+    except Exception as e:
+        print("Es ist folgender Fehler aufgetreten: \n"+e.args[0])
+    except:
+        print("Es ist ein Fehler aufgetreten: ", sys.exc_info()[0])
+
+def seedDB():
+    try:
+        
+    except Exception as e:
+        print("Es ist folgender Fehler aufgetreten: \n"+e.args[0])
+    except:
+        print("Es ist ein Fehler aufgetreten: ", sys.exc_info()[0])
+
+def updateDB():
+    try:
+        
+    except Exception as e:
+        print("Es ist folgender Fehler aufgetreten: \n"+e.args[0])
+    except:
+        print("Es ist ein Fehler aufgetreten: ", sys.exc_info()[0])
+
+
+def main():
+    try:
+        
+    except Exception as e:
+        print("Es ist folgender Fehler aufgetreten: \n"+e.args[0])
+    except:
+        print("Es ist ein Fehler aufgetreten: ", sys.exc_info()[0])
+        
+main()
