58804fe71ec7b2b6942c9e39460d4999b476ac17
spgfn <simon.pietruska@gfn.education>
2025-02-10 15:49:22 +0100

    Add files via upload

--- Database.py
+++ Database.py
@@ -1,1 +1,31 @@
 import sqlite3
+
+db=sqlite3.connect('user.db')
+
+db.execute("""
+    CREATE TABLE Address (
+        ID INTEGER
+        , country TEXT
+        , zip TEXT
+        , city TEXT
+        , street TEXT
+        , snumber TEXT
+        , PRIMARY KEY(ID))""")
+
+db.execute("""
+    CREATE TABLE User (
+        ID INTEGER
+        , username TEXT
+        , name TEXT
+        , firstname TEXT
+        , email TEXT
+        , phone TEXT
+        , iban TEXT
+        , AddressID INTEGER
+        , PRIMARY KEY(ID))""")
+
+db.execute("""
+    CREATE TABLE Admin (
+        ID INTEGER
+        , UserID INTEGER
+        , PRIMARY KEY(ID))""")
