cfd40a7449556f3e9d3a709123b27605c7a9b762
spgfn <simon.pietruska@gfn.education>
2025-02-10 13:18:35 +0100

    Add files via upload

---
+++ Admin.py
@@ -0,0 +1,6 @@
+import User
+User=User.User
+
+class Admin():
+    def __init__(self, User):
+        self.User=User
---
+++ User.py
@@ -0,0 +1,9 @@
+
+class User:
+    def __init__(self, username, name, firstname, email, iban, ):
+        self.username=username
+        self.name=name
+        self.firstname=firstname
+        self.email=email
+        self.iban=iban
+        
