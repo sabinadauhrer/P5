0cfb41af84f73ec30f64d5c07f7b946d62432186
spgfn <simon.pietruska@gfn.education>
2025-02-10 13:46:23 +0100

    Add files via upload

--- User.py
+++ User.py
@@ -1,9 +1,12 @@
+import Address
+Address=Address.Address
 
 class User:
-    def __init__(self, username, name, firstname, email, iban, ):
+    def __init__(self, username, name, firstname, email, iban, Address):
         self.username=username
         self.name=name
         self.firstname=firstname
         self.email=email
         self.iban=iban
+        self.Address=Address
         
