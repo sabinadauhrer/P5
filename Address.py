ffe8202cf7863bd2b2aa12c021ee96471d7f3fea
spgfn <simon.pietruska@gfn.education>
2025-02-10 13:46:23 +0100

    Add files via upload

---
+++ Address.py
@@ -0,0 +1,8 @@
+
+class Address:
+    def __init__(self, zip, aname, street, snumber):
+        self.zip=zip
+        self.aname=aname
+        self.street=street
+        self.snumber=snumber
+        
