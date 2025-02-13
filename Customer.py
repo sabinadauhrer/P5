250cc71fe7a68e883ba555b744c5bf735554177f
spgfn <simon.pietruska@gfn.education>
2025-02-13 14:36:00 +0100

    Add files via upload

--- Customer.py
+++ Customer.py
@@ -17,4 +17,24 @@ class Customer:
         return self.Person
     def setPerson(self, Person=Person):
         self.Person=Person
+    def getName(self):
+        return self.Person.getName
+    def setName(self, name):
+        self.Person.setName=name
+    def getFirstname(self):
+        return self.Person.getFirstname
+    def setFirstname(self, firstname):
+        self.Person.setFirstname=firstname
+    def getEmail(self):
+        return self.Person.getEmail
+    def setEmail(self, email):
+        self.Person.setEmail=email
+    def getPhone(self):
+        return self.Person.getPhone
+    def setPhone(self, phone):
+        self.Person.setPhone=phone
+    def getIBAN(self):
+        return self.Person.getIBAN
+    def setIBAN(self, iban):
+        self.Person.setIBAN=iban
     
--- Database.py
+++ Database.py
@@ -56,19 +56,57 @@ def createDB():
     seedroot()
 
 def seedroot():
+    adata=['root','root','root','root','root']
     db=sqlite3.connect('user.db')
     cur=db.cursor()
+    cur.execute("""INSERT INTO Address (
+        country,
+        zip,
+        city,
+        street,
+        snumber
+        ) VALUES (?, ?, ?, ?, ?);""", adata)
+    db.commit()
+    db.close()
+    
+    db=sqlite3.connect('user.db')
+    cur=db.cursor()
+    cur.execute(
+        "SELECT id FROM Address WHERE zip = ? AND street = ? AND snumber = ?"
+        , ('root','root','root'))
+    AddressID=cur.fetchone()
+    AddressID=AddressID[0]
+    pdata=['root','root','root','root','root',AddressID]
+    cur.execute("""INSERT INTO Person (
+        name,
+        firstname,
+        email,
+        phone,
+        iban,
+        AddressID
+        ) VALUES (?, ?, ?, ?, ?, ?);""", pdata)
+    db.commit()
+    db.close()
+    
+    db=sqlite3.connect('user.db')
+    cur=db.cursor()
+    cur.execute(
+        "SELECT id FROM Person WHERE email = ? AND phone = ? AND iban = ?"
+        , ('root','root','root'))
+    PersonID=cur.fetchone()
+    PersonID=PersonID[0]
+    udata=['root','root',1,PersonID]
     cur.execute("""INSERT INTO User (
         username,
         password,
         adminrole,
         PersonID
-        ) VALUES (root, root, 1, ?);""", )
+        ) VALUES (?, ?, ?, ?);""", udata)
     db.commit()
     db.close()
     
 def seedAddress(Address=Address):
-    data=[Address.getCountry,Address.getZIP,Address.getCity,Address.getStreet,Address.setSnumber]
+    data=[Address.getCountry,Address.getZIP,Address.getCity,Address.getStreet,Address.getSnumber]
     db=sqlite3.connect('user.db')
     cur=db.cursor()
     cur.execute("""INSERT INTO Address (
@@ -82,43 +120,77 @@ def seedAddress(Address=Address):
     db.close()
     
 def seedPerson(Person=Person):
-    data=[Person.getName,Person.getFirstname,Person.getEmail,Person.getPhone,Person.getIBAN]
     db=sqlite3.connect('user.db')
     cur=db.cursor()
-    cur.execute("""INSERT INTO Person (
-        name,
-        firstname,
-        email,
-        phone,
-        iban,
-        AddressID
-        ) VALUES (?, ?, ?, ?, ?, ?);""", data)
-    db.commit()
+    cur.execute(
+        "SELECT id FROM Address WHERE zip = ? AND street = ? AND snumber = ?"
+        , (Person.getAddress,Person.getStreet,Person.getSnumber))
+    AddressID=cur.fetchone()
+    if AddressID:
+        AddressID=AddressID[0]
+        data=[Person.getName,Person.getFirstname,Person.getEmail,Person.getPhone,Person.getIBAN,AddressID]
+        cur.execute("""INSERT INTO Person (
+            name,
+            firstname,
+            email,
+            phone,
+            iban,
+            AddressID
+            ) VALUES (?, ?, ?, ?, ?, ?);""", data)
+        db.commit()
+    else:
+        print(f"No address found with the provided data for {Person.getAddress()}, {Person.getStreet()}, {Person.getSnumber()}.")
     db.close()
     
 def seedUser(User=User):
-    data=[User.getUsername,User.getPassword,0]
     db=sqlite3.connect('user.db')
     cur=db.cursor()
-    cur.execute("""INSERT INTO User (
-        username,
-        password,
-        adminrole,
-        PersonID
-        ) VALUES (?, ?, ?, ?);""", data)
-    db.commit()
+    cur.execute(
+        "SELECT id FROM Person WHERE email = ? AND phone = ? AND iban = ?"
+        , (User.getEmail,User.getPhone,User.getIBAN))
+    PersonID=cur.fetchone()
+    if PersonID:
+        PersonID=PersonID[0]
+        data=[User.getUsername,User.getPassword,0,PersonID]
+        cur.execute("""INSERT INTO User (
+            username,
+            password,
+            adminrole,
+            PersonID
+            ) VALUES (?, ?, ?, ?);""", data)
+        db.commit()
+    else:
+        print(f"No address found with the provided data for {User.getEmail()}, {User.getPhone()}, {User.getIBAN()}.")
     db.close()
     
+def seedUserComplete(User=User,Person=Person,Address=Address):
+    seedAddress(Address)
+    seedPerson(Person)
+    seedUser(User)
+    
 def seedCustomer(Customer=Customer):
-    data=[Customer.getCompany]
     db=sqlite3.connect('user.db')    
     cur=db.cursor()
-    cur.execute("""INSERT INTO Customer (
-        company,
-        PersonID
-        ) VALUES (?, ?);""", data)
-    db.commit()
+    cur.execute(
+        "SELECT id FROM Person WHERE email = ? AND phone = ? AND iban = ?"
+        , (Customer.getEmail,Customer.getPhone,Customer.getIBAN))
+    PersonID=cur.fetchone()
+    if PersonID:
+        PersonID=PersonID[0]
+        data=[Customer.getCompany,PersonID]
+        cur.execute("""INSERT INTO Customer (
+            company,
+            PersonID
+            ) VALUES (?, ?);""", data)
+        db.commit()
+    else:
+        print(f"No address found with the provided data for {Customer.getEmail()}, {Customer.getPhone()}, {Customer.getIBAN()}.")
     db.close()
+
+def seedCustomerComplete(Customer=Customer,Person=Person,Address=Address):
+    seedAddress(Address)
+    seedPerson(Person)
+    seedCustomer(Customer)
     
 def updateDB():
     db=sqlite3.connect('user.db')
