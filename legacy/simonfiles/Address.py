f0d26f35b8b121f5632885b0f8d5c4ca23a554cd
kxern <david.kern@gfn.education>
2025-02-17 10:20:53 +0100

    add simon´s files to legacy

---
+++ legacy/simonfiles/Address.py
@@ -0,0 +1,43 @@
+
+class Address:
+    def __init__(self, country, zip, city, street, snumber):
+        self.country=country
+        self.zip=zip
+        self.city=city
+        self.street=street
+        self.snumber=snumber
+    
+    def getCountry(self):
+        return self.country
+    def setCountry(self, country):
+        self.country=country
+    def newCountry(self):
+        self.setCountry(input("Country:\n"))
+    
+    def getZIP(self):
+        return self.zip
+    def setZIP(self, zip):
+        self.zip=zip
+    def newZIP(self):
+        self.setZIP(input("ZIP:\n"))
+    
+    def getCity(self):
+        return self.city
+    def setCity(self, city):
+        self.city=city
+    def newCity(self):
+        self.setCity(input("City:\n"))
+    
+    def getStreet(self):
+        return self.street
+    def setStreet(self, street):
+        self.street=street
+    def newStreet(self):
+        self.setStreet(input("Street:\n"))
+    
+    def getSnumber(self):
+        return self.snumber
+    def setSnumber(self, snumber):
+        self.snumber=snumber
+    def newSnumber(self):
+        self.setSnumber(input("Streetnumber:\n"))
