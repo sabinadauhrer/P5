c7971b2852d8405a00d7e94688ff157c212f8c16
David Kern <d.kern@mail.com>
2025-02-18 06:35:22 +0100

    config added

---
+++ config.py
@@ -0,0 +1,17 @@
+# Setup root for seed
+root_username = 'root'
+root_password = 'root'
+root_email = 'root@root.com'
+
+# Session lifetime
+session_lifetime = 30
+
+# 2FA on/off
+two_fa_on = False
+
+# 2FA SMTP
+mail_server = 'smtp.example.com'
+mail_port = 587
+mail_use_tls = False
+mail_username = 'your-email@example.com'
+mail_password = 'your-email-password'
